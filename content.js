806d987189defd37b07b6b86b900515d155acfc7
Fraser Morrison <frazemor@hotmail.com>
2023-01-25 00:01:16 -0500

    Changed Button query to reflect site style change

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const promptBoxQuery = '.text-sm.relative.font-medium.rounded-lg.overflow-auto'
 const promptTitleQuery = '.overflow-x-auto.relative.hide-scrollbar.border-b.flex'
 
-const buttonQuery = '.sc-kgTSHT.cFBAZz.MuiButtonBase-root.px-4.py-1.relative.no-underline.font-light.opacity-40'
+const buttonQuery = '.sc-kgTSHT.cFBAZz.MuiButtonBase-root.px-4.py-1.relative.no-underline'
 
 const urlQuery = 'a.underline.flex.items-center.truncate'
 
