f9c8574abd332ff6d68463d805fb48774693f045
Fraser Morrison <frazemor@hotmail.com>
2023-02-07 03:39:20 -0500

    fixed link button & added padding to buttons

--- content.js
+++ content.js
@@ -108,7 +108,7 @@ const addIcon = async (promptText = '', artId) => {
     copyIcon.className = 'prompt-copy';
 
     const linkIcon = document.createElement('div');
-    linkIcon.innerHTML = `<a href="${artId}"><svg class="sc-ipEyDJ ewkAmK MuiSvgIcon-root MuiSvgIcon-fontSizeMedium" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ContentCopyIcon"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path></svg></a>`;
+    linkIcon.innerHTML = `<a href="artwork/${artId}"><svg class="sc-ipEyDJ ewkAmK MuiSvgIcon-root MuiSvgIcon-fontSizeMedium" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ContentCopyIcon"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path></svg></a>`;
     linkIcon.className = 'prompt-link';
     linkIcon.style.cursor = 'pointer';
     
